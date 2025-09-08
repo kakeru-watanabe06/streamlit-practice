3326d0f40600672c103af27373af28af9eed8dcb
kakeru-watababe06 <watanabe-kakeru-jt@ynu.jp>
2025-09-08 11:07:07 +0900

    計算機能修正

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.title("簡易計算機能")
 st.write("任意の二つの数字について四則演算の実行ができます。")
 
-num1 = st.number_input("1つ目の数字を入力してください", value=0)
-num2 = st.number_input("2つ目の数字を入力してください", value=0)
+num1 = st.number_input("1つ目の数字を入力してください", value=0.0, step=0.1)
+num2 = st.number_input("2つ目の数字を入力してください", value=0.0, step=0.1)
 operation = st.selectbox("演算子を選択してください", ("+", "-", "*", "/"))
 if st.button("計算実行"):
     if operation == "+":
